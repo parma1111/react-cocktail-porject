52d014bfd7ce9bb2fe4bbb58a1e7d8d46a10c779
parma1111 <parmarambuldak800@gmail.com>
2022-11-07 11:39:05 +0530

    update

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,7 @@ export default function About() {
       <p>
          Hi I am Parma, founder & ceo of cocktail. So cocktail deals in many types
          of cocktails.Here you can find all the cocktails of world.It is founded in 1998.
-         Ii gives you a specail test of cocktail that you can not find anywere in the 
+         It gives you a specail test of cocktail that you can not find anywere in the 
          world.Because it cares about customer reviews and there needs.
       </p>
     </section>
